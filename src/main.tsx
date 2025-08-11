a6a5b58960557dda4ed14efbd8c0b41249f75cff
mstne03 <mserraitorne@gmail.com>
2025-08-11 13:32:20 +0200

    feat: creates pages components and routing, and also header component to resolve #1

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './app/App.tsx'
+import Charts from './pages/Charts/Charts'
+import Map from './pages/Map/Map'
+import FullCalendar from './pages/FullCalendar/FullCalendar'
+import NotFound from './pages/NotFound/NotFound'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 const router = createBrowserRouter([
-  {path:"/", element: <App/>}
+  {path:"/", element: <App/>},
+  {path:"/map", element: <Map/>},
+  {path:"/charts", element: <Charts/>},
+  {path:"/calendar", element: <FullCalendar/>},
+  {path:"*", element: <NotFound/>}
 ]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
